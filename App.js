3e82a544b1d92d732c6f046a681268757807ee7f
Javel Wilson <javelawilson@gmail.com>
2021-05-12 17:01:29 -0500

    Define main app features and their screens

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
 import { StatusBar } from 'expo-status-bar'
 import React, { useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import * as SplashScreen from 'expo-splash-screen'
 
 export default function App() {
-  const prepare = async () => {
-    try {
-      await SplashScreen.preventAutoHideAsync()
-    } catch (err) {
-      console.log(err.message)
-    }
-  }
-
-  useEffect(() => {
-    prepare()
-  }, [])
-
   return (
     <View style={styles.container}>
       <Text>Wah Gwan!</Text>
---
+++ src/features/routes/screens/route-detail.screen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export const RouteDetailScreen = () => {
+  return (
+    <View>
+      <Text>RouteDetailScreen</Text>
+    </View>
+  )
+}
