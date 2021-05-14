b5c0d7fdfd086a8a62870d09e261a526e3770f59
Javel Wilson <javelawilson@gmail.com>
2021-05-13 19:00:00 -0500

    Simulate authentication flow

--- src/infrastructure/navigation/index.js
+++ src/infrastructure/navigation/index.js
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { AccountNavigator } from './account.navigator'
 import { AppNavigator } from './app.navigator'
+import * as SplashScreen from 'expo-splash-screen'
 
 export const Navigation = () => {
+  const [isAppReady, setIsAppReady] = useState(false)
+  const [isSignedIn, setIsSignedIn] = useState(false)
+
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync()
+        const token = await new Promise((resolve, reject) => {
+          setTimeout(() => resolve('token'), 3000)
+        })
+        if (token) {
+          setIsSignedIn(true)
+        }
+      } catch (error) {
+        console.warn(error.message)
+      } finally {
+        setIsAppReady(true)
+      }
+    }
+    prepare()
+  }, [])
+
+  const onLayoutRootView = useCallback(async () => {
+    if (isAppReady) {
+      await SplashScreen.hideAsync()
+    }
+  }, [isAppReady])
+
+  if (!isAppReady) {
+    return null
+  }
+
   return (
-    <NavigationContainer>
-      <AppNavigator />
+    <NavigationContainer onReady={onLayoutRootView}>
+      {isSignedIn ? <AppNavigator /> : <AccountNavigator />}
     </NavigationContainer>
   )
 }
